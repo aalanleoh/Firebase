ceb7d1eedbe8d229dd7e955022cebe1e0e1372eb
aleon <alanantonioleonhernandez@gmail.com>
2025-10-22 17:23:30 -0600

    Pantalla Sign Up funcionando de forma básica con firebase

--- app/src/main/java/com/aalh/firebase/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/aalh/firebase/presentation/signup/SignUpScreen.kt
@@ -1,9 +1,93 @@
 package com.aalh.firebase.presentation.signup
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aalh.firebase.R
+import com.aalh.firebase.ui.theme.Black
+import com.aalh.firebase.ui.theme.SelectedTheme
+import com.aalh.firebase.ui.theme.UnselectedTheme
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun SignUpScreen(auth: FirebaseAuth) {
-
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Black)
+            .padding(horizontal = 32.dp, vertical = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Icon(
+                painter = painterResource(R.drawable.ic_backl_24),
+                contentDescription = "",
+                tint = White,
+                modifier = Modifier
+                    .padding(vertical = 24.dp)
+                    .size(24.dp)
+            )
+            Spacer(Modifier.weight(1f))
+        }
+        Text("Email or username", color = White, fontWeight = FontWeight.Bold, fontSize = 40.sp)
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = UnselectedTheme,
+                focusedContainerColor = SelectedTheme
+            )
+        )
+        Spacer(Modifier.height(48.dp))
+        Text("Password", color = White, fontWeight = FontWeight.Bold, fontSize = 40.sp)
+        TextField(
+            value = password, onValueChange = { password = it },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = UnselectedTheme,
+                focusedContainerColor = SelectedTheme
+            )
+        )
+        Spacer(Modifier.height(48.dp))
+        Button(onClick = {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "Se creo el usuario", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Se genero un error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }) {
+            Text(text = "Sign Up")
+        }
+    }
 }
